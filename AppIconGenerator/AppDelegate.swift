3267df862306754f735924bc68dbd457ea9c40c3
Joseph Baraga <jbaraga@stpaulrad.com>
2018-12-30 09:10:09 -0600

    Initial Commit

---
+++ AppIconGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppIconGenerator
+//
+//  Created by Joseph Baraga on 12/30/18.
+//  Copyright © 2018 Joseph Baraga. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
